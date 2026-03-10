fe68275b8174025190c75fa5a8f6d722e4b6fa11
JallalCihuy <jallaludin240@gmail.com>
2026-03-11 06:24:11 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mysql = require("mysql2");
+const bodyPerser = require("body-parser");
+const app = express();
+const log = console.log();
+
+app.use(bodyPerser.urlencoded({ extended:true }))
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database:"TabunganMyGuweh"
+    }
+);
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/simpan', (req,res) => {
+    const { NamaPenabung,nominal } = req.body;
+    const sql =  "INSERT INTO Nabung (NamaPenabung,nominal) VALUES (?,?)"
+
+    db.query(sql,[NamaPenabung,nominal], (err,result) => {
+        if(err) throw err;
+        res.send("giluyyy berhasil cuyy");
+    });
+});
+
+app.listen(67, () => console.log("berjalan di localhost:67"));
